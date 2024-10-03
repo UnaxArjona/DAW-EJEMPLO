98f8df9ff00826aa91a89d2ca9cdbfd26b70eddc
uarjona <unaxarjona@ceroca.cat>
2024-10-03 16:05:14 +0200

    generadorNumero

---
+++ Assets/Scrips/GeneradoNumeros.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneradoNumeros : MonoBehaviour
+{
+
+    public GameObject prefabNum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("GenerarNumero",1f, 2f);
+
+
+    }
+
+    private void GenerarNumero()
+    {
+    
+
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+       
+    }
+}
